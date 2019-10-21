f9f195499379fa2517519b74e6ea842d39ae3c4c
Sulaiman Abolaji Bisiriyu <abolajibisiriyu@gmail.com>
2019-10-21 21:09:58 +0100

    updated type definition

--- build/index.d.ts
+++ build/index.d.ts
@@ -2,11 +2,19 @@ declare module "sly-rc-paginator" {
   import React from "react";
 
   export interface PaginatorProps {
-    options: {
+    meta: {
       totalItems: number;
       currentPage: number;
       itemsPerPage: number;
     };
+    options: {
+      ulClassName: string;
+      liClassName: string;
+      activeClassName: string;
+      disabledClassName: string;
+      anchorClassName: string;
+    };
+    onPageChange: (page: number) => void;
     prevComponent: Function | string;
     nextComponent: Function | string;
     firstComponent: Function | string;
